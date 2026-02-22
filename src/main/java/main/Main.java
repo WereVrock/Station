194e262b1cd849d38f4962f7c124088e4df39053
bugrabeyaz <bugrabeyaz@yahoo.com>
2026-02-22 07:51:32 +0200

    game exposed

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,9 +7,10 @@ import javax.swing.SwingUtilities;
 
 public class Main {
 
+    public static Game game;
     public static void main(String[] args) {
 
-        Game game = new Game();
+        game = new Game();
 
         try {
             game.loadItems("items.json");
